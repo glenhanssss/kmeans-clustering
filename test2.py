df2fe7b4207d1f6b3e53f952692661be745555c7
glen anak baik <126223055+glenhanssss@users.noreply.github.com>
2024-11-17 22:19:37 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -243,11 +243,11 @@ if dataset:
                 st.pyplot(plt)
                 st.markdown("<br><br>", unsafe_allow_html=True)
 
-            # Future Feature Text
-            st.markdown("""<hr style="height:4px;border:none;color:#333;background-color:#333;" />""", unsafe_allow_html=True)
-            st.title("Future Feature with Generative AI")
-            st.markdown("<strong>Form Input based on Clustering Result</strong>", unsafe_allow_html=True)
-            st. image(r"images/4. AIchemist generate.png")
-            st.markdown("<br>", unsafe_allow_html=True)
-            st.markdown("<strong>Recommendation and Material Result based on Form</strong>", unsafe_allow_html=True)
-            st.image(r"images/5. AIchemist generate -result.png")
+            # # Future Feature Text
+            # st.markdown("""<hr style="height:4px;border:none;color:#333;background-color:#333;" />""", unsafe_allow_html=True)
+            # st.title("Future Feature with Generative AI")
+            # st.markdown("<strong>Form Input based on Clustering Result</strong>", unsafe_allow_html=True)
+            # st. image(r"images/4. AIchemist generate.png")
+            # st.markdown("<br>", unsafe_allow_html=True)
+            # st.markdown("<strong>Recommendation and Material Result based on Form</strong>", unsafe_allow_html=True)
+            # st.image(r"images/5. AIchemist generate -result.png")
